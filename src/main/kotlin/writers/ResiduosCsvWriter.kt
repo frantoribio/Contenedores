dd11ad06c8b47392ce971590c2d567b1c596b710
Bank <robertobl022@gmail.com>
2022-09-29 20:23:34 +0200

    Cleanup

--- src/main/kotlin/writers/ResiduosCsvWriter.kt
+++ src/main/kotlin/writers/ResiduosCsvWriter.kt
@@ -8,13 +8,9 @@ import java.nio.file.Files
 class ResiduosCsvWriter : Writer<Residuos> {
     private val mapper = CsvToResiduos()
 
-    override fun write(path: String, content: Sequence<Residuos>) {
-        File(path).apply {
-            if (exists())
-                Files.delete(toPath())
-            createNewFile()
-        }.printWriter().use { out ->
-            mapper.map(content).forEach { out.println(it) }
-        }
-    }
+    override fun write(path: String, content: Sequence<Residuos>) = File(path)
+        .apply { if (exists()) Files.delete(toPath()) }
+        .apply { createNewFile() }
+        .printWriter()
+        .use { out -> mapper.map(content).forEach { out.println(it) } }
 }
