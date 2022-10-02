9d141fc4eee4c36619a17156d9779477e46cfcb1
Francisco Toribio Respaldo <90746957+frantoribio@users.noreply.github.com>
2022-10-02 21:16:29 +0200

    mapeador de csv de contenedores

--- src/main/kotlin/mappers/contenedores/CsvMapperContenedores.kt
+++ src/main/kotlin/mappers/contenedores/CsvMapperContenedores.kt
@@ -1,23 +1,69 @@
 package mappers.contenedores
 
-import exceptions.CsvException
 import extensions.*
 import models.Contenedor
-import models.Residuo
-import java.time.LocalDate
-import java.time.Month
-import java.util.*
+import exceptions.CsvException
+
 
 /**
  * maps all lines of the csv file to a Residuo lazy sequence
  */
 
-class CsvMapperContenedores {
-    fun mapTo(input: Sequence<String>): Sequence<Unit> = input.drop(1).map { line ->
+object CsvMapperContenedores {
+    fun mapTo(input: Sequence<String>): Sequence<Contenedor> = input.drop(1).map { line ->
+
         val (codIntSitu, tipoContenedor, modelo, descripModelo,
             cantidadContenedores, lote, distrito, tipoVia, nombreVia,
-            numVia, longitud, latitud, direccion
+            numVia, coordenadaX, coordenadaY, latitud, longitud, direccion
         ) = line.split(';')
+
+    Contenedor(
+
+        codIntSitu = codIntSitu?.ifBlank { throw CsvException("El codigo no puede estar vacio") }
+            ?: throw CsvException("El codigo no puede ser nulo"),
+
+        tipoContenedor = tipoContenedor?.ifBlank { throw CsvException("El tipo del contenedor nu puede estar vacío") }
+            ?: throw CsvException("El tipo no puede ser nulo"),
+
+        modelo = modelo?.ifBlank { throw CsvException("El modelo no puede estar vacio") }
+            ?: throw CsvException("El modelo no puede ser nulo"),
+
+        descripModelo = descripModelo?.ifBlank { throw CsvException("La descripción no puede quedar vacía") }
+            ?: throw CsvException("La descripción no puede ser nulo"),
+
+        cantidadContenedores = cantidadContenedores?.toIntOrNull()
+            ?: throw CsvException("Las cantidades no son números"),
+
+        lote = lote?.toIntOrNull()
+            ?: throw CsvException("El lote no es un número"),
+
+        distrito = distrito?.ifBlank { throw CsvException("El distrito no puede quedar vacío") }
+            ?: throw CsvException("El distrito no puede ser nulo"),
+
+        tipoVia = tipoVia?.ifBlank { throw CsvException("El tipo de vía no puede quedar vacía") }
+            ?: throw CsvException("El tipo de vía no puede ser nulo"),
+
+        nombreVia = nombreVia?.ifBlank { throw CsvException("El nombre de vía no puede quedar vacía") }
+            ?: throw CsvException("El nombre de vía no puede ser nulo"),
+
+        numVia = numVia?.toIntOrNull()
+            ?: throw CsvException("El numero de vía no es un número"),
+
+        coordenadaX = coordenadaX?.toLongOrNull()
+            ?: throw CsvException("La coordenada no es un número"),
+
+        coordenadaY = coordenadaY?.toLongOrNull()
+            ?: throw CsvException("La coordenada no es un número"),
+
+        longitud = longitud?.toIntOrNull()
+            ?: throw CsvException("La longitud no es un número"),
+
+        latitud = latitud?.toIntOrNull()
+            ?: throw CsvException("La latitud no es un número"),
+
+        direccion = direccion?.ifBlank { throw CsvException("La dirección no puede quedar vacía") }
+            ?: throw CsvException("El dirección no puede ser nula")
+    )
 
 
     }
--- src/main/kotlin/readers/ContenedoresCsvReader.kt
+++ src/main/kotlin/readers/ContenedoresCsvReader.kt
@@ -6,10 +6,10 @@ import java.io.File
 import java.io.FileNotFoundException
 
 class ContenedoresCsvReader (private val path: String) : Reader<Contenedor> {
-    private val mapper = CsvMapperContenedores()
+    private val mapper = CsvMapperContenedores
     override fun read(): Sequence<Contenedor> = sequence {
        File(path)
            .apply { if (!exists()) throw FileNotFoundException() }
-           .useLines {yieldAll(mapper.mapTo(it))}
+           .useLines { yieldAll(mapper.mapTo(it))}
     }
 }
