d7bd79b05326400b9b1d7e60e32915dcef0b261d
Roberto Blázquez <elroby444@gmail.com>
2022-10-09 17:19:20 +0200

    Add more checks

--- src/main/kotlin/readers/CsvDirectoryReader.kt
+++ src/main/kotlin/readers/CsvDirectoryReader.kt
@@ -10,10 +10,12 @@ class CsvDirectoryReader<T>(path: String, private val parser: CsvParser<T>) :
     private var fileReader: FileReader<T>
 
     init {
-        val csvFiles = File(path).listFiles { _, name -> name.endsWith(".csv") }
-        val firstLines =
-            csvFiles?.map { file -> file to file.firstLine }
-                ?: throw IllegalArgumentException("No se encontraron archivos .csv en el directorio $path")
+        val csvFiles = File(path)
+            .apply { if (!isDirectory) throw IllegalArgumentException("$path no es un directorio") }
+            .listFiles { _, name -> name.endsWith(".csv") }
+
+        val firstLines = csvFiles?.map { file -> file to file.firstLine }
+            ?: throw IllegalArgumentException("No se encontraron archivos .csv en el directorio $path")
 
         val file = firstLines.firstOrNull { it.second == parser.firstLine }
             ?: throw IllegalArgumentException("No se encontró el archivo con el formato correcto")
