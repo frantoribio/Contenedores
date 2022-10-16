ded955180310aa6ddc93d7f1e346d476a1c6ad6c
Roberto Blázquez <elroby444@gmail.com>
2022-10-16 16:13:56 +0200

    Better loggin

--- src/main/kotlin/loggers/LoggerCsvDirectoryReader.kt
+++ src/main/kotlin/loggers/LoggerCsvDirectoryReader.kt
@@ -6,8 +6,8 @@ import readers.CsvDirectoryReader
 class LoggerCsvDirectoryReader<T>(private val reader: CsvDirectoryReader<T>, private val logger: KLogger) {
     suspend fun read(): Sequence<T> {
         logger.info("Reading ${reader.path}")
-        logger.info("Importers: ${reader.parser::class.simpleName}")
-        logger.info("Formats ${reader.parser.extension}")
+        logger.info("   Importers: ${reader.parser::class.simpleName}")
+        logger.info("   Formats ${reader.parser.extension}")
         return reader.read()
     }
 }
--- src/main/kotlin/loggers/LoggerDirectoryWritter.kt
+++ src/main/kotlin/loggers/LoggerDirectoryWritter.kt
@@ -7,8 +7,8 @@ import writers.IWriter
 class LoggerDirectoryWritter<T>(private val writer: DirectoryWriter<T>, private val logger: KLogger) : IWriter<T> {
     override suspend fun write(content: T) {
         logger.info("Writing ${writer.fileName} to ${writer.path}")
-        logger.info("Formats ${writer.exporters.joinToString(separator = " ") { it.extension }}")
-        logger.info("Exporters: ${writer.exporters.map { it::class.simpleName }.joinToString(" ")}")
+        logger.info("   Formats ${writer.exporters.joinToString(separator = " ") { it.extension }}")
+        logger.info("   Exporters: ${writer.exporters.map { it::class.simpleName }.joinToString(" ")}")
         writer.write(content)
     }
 }
